381ccfd8936bc9b540443cd988facf635608b828
Alexsander Garachun <garachun.o@gmail.com>
2023-01-13 13:00:53 +0300

    calculator.js

--- calculator/calculator.js
+++ calculator/calculator.js
@@ -4,54 +4,11 @@ const OPERATIONS = {
     multiplication: "*",
     division: "/"
   };
-  //hello
+  
   function calculate({a, b, operation}){
     let result = null;
-    const OPERATIONS = {
-        sum (a, b) {result = a + b
-        return result},
-        
-        substruction(a,b) {result = a - b
-        return result},
-        
-        multiplication(a,b) {result = a * b
-        return result},
-
-        division(a, b) {result = a / b
-        return result}
-
-    }
-     return result;
-    }
-
-    
-   /* const  = [
-        result = sum(a, b)
-        
-    ]
-
-    const arr2 = [
-        result = substraction(a, b)
-    ]
-
-    const arr3 = [
-        result = multiplication(a, b)
-        
-    ]
-
-    const arr4 = [
-        result = division(a, b)
-    ]
-    
-    const arrConcat1 = arr1.concat(arr2);
-    const arrConcat2 = arr2.concat(arr2);
-    const arrConcat3 = arr3.concat(arr4);
-    const arrConcat4 = [...arr1, ...arr2, ...arr3, ...arr4];
-    */
-    
-    
-
-/*  switch (operation) {
+  
+    switch (operation) {
         case OPERATIONS.sum:
             result = sum(a, b);
             break;
@@ -73,116 +30,10 @@ const OPERATIONS = {
             break;
     }
     
-   */
-
-
-  /*
-    const arr1 = [
-        result = sum(a, b)
-        
-    ]
-
-    const arr2 = [
-        result = substraction(a, b)
-    ]
-
-    const arr3 = [
-        result = multiplication(a, b)
-        
-    ]
-
-    const arr4 = [
-        result = division(a, b)
-    ]
-    
-    const arrConcat1 = arr1.concat(arr2);
-    const arrConcat2 = arr2.concat(arr2);
-    const arrConcat3 = arr3.concat(arr4);
-    const arrConcat4 = [...arr1, ...arr2, ...arr3, ...arr4];
-    
-    */
-    
-
-
-/*let operation 
-        'sum': function () {
-            return sum(a, b);
-        }
-    */
-
-
-
-
-
-
-    
-   /* switch (operation) {
-        case OPERATIONS.sum:
-            result = sum(a, b);
-            break;
-        
-        case OPERATIONS.substruction:
-            result = substraction(a, b);
-            break;
-        
-        case OPERATIONS.multiplication:
-            result = multiplication(a, b);
-            break;
-  
-        case OPERATIONS.division:
-            result = divison(a, b);
-            break;
-  
-        
-        default:
-            break;
-    }
-    
-   */
-    
-
-
-
-
-/*
-function calculate({a, b, objOPERATIONS}){
-    let result = null;
    
-    const objsum = [
-    result = sum(a, b)
-    ]
+    return result;
+  }
 
-    const objsubstruction = [
-    result = substruction(a, b)
-    ]
     
     
-    objOPERATIONS = Object.assign({}, objsum, objsubstruction);
-     result = {...objsum, ...objsubstruction} 
-    */
-
-/*
-     const arr1 = [
-        result = sum(a, b)
-    ]
-
-    const arr2 = [
-        result = substraction(a, b)
-    ]
-
-    const arr3 = [
-        result = multiplication(a, b)
-    ]
-
-    const arr4 = [
-        result = division(a, b)
-    ]
-    
-    const arrConcat1 = arr1.concat(arr2);
-    const arrConcat2 = arr2.concat(arr2);
-    const arrConcat3 = arr3.concat(arr4);
-    const arrConcat4 = [...arr1, ...arr2, ...arr3, ...arr4];
-
-
-*/
 
