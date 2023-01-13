b154f49558757f77e254295f07987b23a24f7fef
aleksandrgarachun <115419603+aleksandrgarachun@users.noreply.github.com>
2023-01-13 18:43:34 +0300

    Update calculator.js

--- calculator/calculator.js
+++ calculator/calculator.js
@@ -1,38 +1,61 @@
 const OPERATIONS = {
-    sum: "+",
-    substruction: "-",
-    multiplication: "*",
-    division: "/"
-  };
-  
+    '+': (a, b) => a + b,
+    '-': (a, b) => a - b,
+    '*': (a, b) => a * b,
+    '/': (a, b) => a / b
+};
+
   function calculate({a, b, operation}){
-    let result = null;
-  
-    switch (operation) {
-        case OPERATIONS.sum:
-            result = sum(a, b);
-            break;
-        
-        case OPERATIONS.substruction:
-            result = substraction(a, b);
-            break;
-        
-        case OPERATIONS.multiplication:
-            result = multiplication(a, b);
-            break;
-  
-        case OPERATIONS.division:
-            result = divison(a, b);
-            break;
-  
-        
-        default:
-            break;
-    }
     
-   
-    return result;
+    const fun = OPERATIONS[operation];
+    return fun(a, b); 
+
   }
+
+      
+      /*    Старый код
+const OPERATIONS = {
+  sum: "+",
+  substruction: "-",     
+  multiplication: "*",
+  division: "/"
+};
+
+function calculate({a, b, operation}){
+  let result = null;
+
+  switch (operation) {
+      case OPERATIONS.sum:
+          result = sum(a, b);
+          break;
+      
+      case OPERATIONS.substruction:
+          result = substraction(a, b);
+          break;
+      
+      case OPERATIONS.multiplication:
+          result = multiplication(a, b);
+          break;
+
+      case OPERATIONS.division:
+          result = divison(a, b);
+          break;
+
+      
+      default:
+          break;
+  }
+  
+ 
+  return result;
+}
+*/
+      
+    
+      
+      
+      
+
 
     
     
